30181c2ab23b905189beb71e393d4a1e42950547
youness <younesserbai@gmail.com>
2024-09-26 18:27:10 +0200

    remove pulic file  and add db/.env to gitignore

--- server.js
+++ server.js
@@ -35,14 +35,15 @@ app.use(express.json());
 // app.use(cookieParser());
 
 //serve static files
-app.use('/', express.static(path.join(__dirname, '/public')));
 
 // routes
-app.use('/', require('./routes/root'));
 app.use('/register', require('./routes/register'));
 app.use('/auth', require('./routes/auth'));
 app.use('/refresh', require('./routes/refresh'));
 app.use('/logout', require('./routes/logout'));
+app.get('/',(req,res)=>{
+    res.send('welcome');
+})
 
 app.use(verifyJWT);
 app.use('/employees', require('./routes/api/employees'));

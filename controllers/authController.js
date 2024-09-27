636ef7ccff32a7770c95d5b46c7762ff4b0bb3cc
youness <younesserbai@gmail.com>
2024-09-27 16:45:27 +0200

    changes added

--- controllers/authController.js
+++ controllers/authController.js
@@ -12,6 +12,9 @@ require('dotenv').config();
 const fsPromises = require('fs').promises;
 const path = require('path');
 
+
+// handleLogin service 
+
 const handleLogin = async (req, res) => {
     const { user, pwd } = req.body;
     if (!user || !pwd) return res.status(400).json({ 'message': 'Username and password are required.' });
